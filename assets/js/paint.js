72c62d42cd05533557e8d4d80725e04b04d19835
Juan Du <duju@oregonstate.edu>
2017-05-22 09:35:12 -0500

    Update paint.js

--- assets/js/paint.js
+++ assets/js/paint.js
@@ -166,7 +166,7 @@ function averageFilter(array,size){
   var thumbheight = 16;
   var xscale = (thumbwidth+0.0) / width;
   var yscale = (thumbheight+0.0) / height;
-  var threshold = 0.55 / (xscale * yscale);
+  var threshold = 0.6 / (xscale * yscale);
   var yend = 0.0;
   for (var f = 0; f < thumbheight; f+=1) // y on output
   {
